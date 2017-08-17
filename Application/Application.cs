b044e96192718128b4fde317502dd92d36f5c896
WorkplaceX <info@workplacex.com>
2017-08-17 19:25:16 +0200

    Update for Hello World

--- Application/Application.cs
+++ Application/Application.cs
@@ -1,9 +1,13 @@
 ﻿namespace Application
 {
+    using System;
     using Framework.Application;
 
     public class AppMain : App
     {
-
+        protected override Type TypePageMain()
+        {
+            return typeof(PageMain);
+        }
     }
 }
---
+++ Application/Page.cs
@@ -0,0 +1,13 @@
+﻿using Framework.Application;
+using Framework.Component;
+
+namespace Application
+{
+    public class PageMain : Page
+    {
+        protected override void InitJson(App app)
+        {
+            new Label(this) { Text = "Hello World!" };
+        }
+    }
+}
