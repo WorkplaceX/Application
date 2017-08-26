85479e2ea63dd0557cf314048d0c74fcc494d2d0
WorkplaceX <info@workplacex.com>
2017-08-27 00:00:38 +0200

    IndicatorFilter

--- Application/Database/Database.cs
+++ Application/Database/Database.cs
@@ -4,11 +4,26 @@
     using System;
     using Framework.Application;
 
+    public partial class HelloWorld_Text
+    {
+        protected override void CellCssClass(App app, string gridName, string index, ref string result)
+        {
+            if (UtilApplication.IndexEnumFromText(index) == IndexEnum.Filter)
+            {
+                result = "indicatorFilter";
+            }
+        }
+    }
+
     public partial class HelloWorld_Number
     {
         protected override void CellCssClass(App app, string gridName, string index, ref string result)
         {
-            if (Row != null)
+            if (UtilApplication.IndexEnumFromText(index) == IndexEnum.Filter)
+            {
+                result = "indicatorFilter";
+            }
+            if (UtilApplication.IndexEnumFromText(index) == IndexEnum.Index)
             {
                 if (Row.Number > 0)
                 {
