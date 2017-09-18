05d009799c00b6919ea4aecd156d283d3cac8dd9
WorkplaceX <info@workplacex.com>
2017-09-18 17:58:15 +0200

    Change from html inline block to float left

--- Application/Page.cs
+++ Application/Page.cs
@@ -8,8 +8,12 @@ namespace Application
     {
         protected override void InitJson(App app)
         {
-            new Label(this) { Text = "Hello World!" };
-            // new GridField(this, null, null, null);
+            Div div = new Div(this);
+            new Label(div) { Text = "Hello World!", Css = "floatLeft" };
+            new Label(div) { Text = "Hello World2!", Css = "floatLeft" };
+            new Button(div) { Text = "Click", Css = "floatLeft" };
+            new Literal(div) { TextHtml = "<b>Bold</b>" };
+            new GridField(this, null, null, null);
             new Grid(this, "Grid1");
             app.GridData.LoadDatabase<HelloWorld>("Grid1");
         }
