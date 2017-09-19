be1caacc72f27241fad157fa36850687d1cbfda0
WorkplaceX <info@workplacex.com>
2017-09-19 14:20:18 +0200

    Skyscraper

--- Application/Page.cs
+++ Application/Page.cs
@@ -8,13 +8,19 @@ namespace Application
     {
         protected override void InitJson(App app)
         {
+            new Grid(this, "Grid1");
             Div div = new Div(this);
             new Label(div) { Text = "Hello World!", Css = "floatLeft" };
             new Label(div) { Text = "Hello World2!", Css = "floatLeft" };
             new Button(div) { Text = "Click", Css = "floatLeft" };
             new Literal(div) { TextHtml = "<b>Bold</b>" };
             new GridField(this, null, null, null);
-            new Grid(this, "Grid1");
+            // Skyscraper
+            var skyscraper = new Div(this) { Css = "backgroundColorBlue" };
+            new Label(skyscraper) { Text = "Skyskraper Label", Css = "backgroundColorGreen" };
+            new GridField(skyscraper, "Grid1", "Text", null) { Css = "backgroundColorGreen" };
+            new GridField(skyscraper, "Grid1", "Number", null) { Css = "backgroundColorGreen" };
+            // Load
             app.GridData.LoadDatabase<HelloWorld>("Grid1");
         }
     }
