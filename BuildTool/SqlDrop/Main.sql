5b4ef05f4e10c9a2f222faa392f999040f91c8d5
unknown <info@workplacex.com>
2017-12-08 18:50:32 +0100

    Update to .NET Core 2.0 and Node 8.9.2 LTS

---
+++ BuildTool/SqlDrop/Main.sql
@@ -0,0 +1,11 @@
+﻿IF NOT EXISTS(SELECT * FROM sys.tables WHERE name = 'HelloWorld') RETURN
+
+ALTER TABLE [dbo].[Attribute] 
+DROP CONSTRAINT FK_X
+
+ALTER TABLE [dbo].[AttributeNote]
+DROP CONSTRAINT FX_Y
+
+DROP TABLE [dbo].[HelloWorld]
+DROP TABLE [dbo].[Attribute]
+DROP TABLE [dbo].[AttributeNote]
