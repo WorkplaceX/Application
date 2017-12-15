2c2a7db31e2e7e163bff38e506a2aa1262c3e8f4
WorkplaceX <info@workplacex.com>
2017-12-15 09:54:22 +0100

    ConnectionStringFramework

--- Application/Page.cs
+++ Application/Page.cs
@@ -12,6 +12,8 @@ namespace Application
             new Label(this) { Text = "Application Start" };
             new Grid(this, new GridName<HelloWorld>());
             app.GridData.LoadDatabase(new GridName<HelloWorld>());
+            new Label(this) { Text = "VersionServer=" + UtilFramework.VersionServer };
+
         }
     }
 }
