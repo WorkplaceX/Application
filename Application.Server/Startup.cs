a10080ac1b6581d80037e92628d03121ba5bcaaf
WorkplaceX <info@workplacex.com>
2018-07-24 08:05:29 +0200

    Rename to Framework.Server

--- Application.Server/Startup.cs
+++ Application.Server/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Server
+namespace Application.Server
 {
     public class Startup
     {
