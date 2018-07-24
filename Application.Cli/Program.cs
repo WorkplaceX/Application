10a9af624787da7d46eb3c9aaf6647e8c9d1cb38
WorkplaceX <info@workplacex.com>
2018-07-24 07:53:15 +0200

    Cli

--- Application.Cli/Program.cs
+++ Application.Cli/Program.cs
@@ -1,12 +1,14 @@
-﻿using System;
-
-namespace Application.Cli
+﻿namespace Application.Cli
 {
+    using Framework.Cli;
+    using System;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UtilCli.Run(args);
+            Console.ReadLine();
         }
     }
 }
