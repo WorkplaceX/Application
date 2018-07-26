375e72488a5c96550f92c5cd6b8ea4d5d251adf9
WorkplaceX <info@workplacex.com>
2018-07-26 17:52:03 +0200

    Cli build client

--- Application.Server/Startup.cs
+++ Application.Server/Startup.cs
@@ -27,7 +27,7 @@ namespace Application.Server
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World5!");
+                await context.Response.WriteAsync("Hello World6!");
             });
         }
     }
