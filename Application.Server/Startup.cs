3eb10381129712451a4cba9016bb298517a3a81a
WorkplaceX <info@workplacex.com>
2018-07-26 11:13:20 +0200

    Cli deploy

--- Application.Server/Startup.cs
+++ Application.Server/Startup.cs
@@ -27,7 +27,7 @@ namespace Application.Server
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World2!");
             });
         }
     }
