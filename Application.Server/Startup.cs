f6de1448a89ae7f52b7c08b5a95aaa39bce97b18
WorkplaceX <info@workplacex.com>
2018-07-27 09:22:03 +0200

    Cli build client to server

--- Application.Server/Startup.cs
+++ Application.Server/Startup.cs
@@ -20,15 +20,7 @@ namespace Application.Server
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World7!");
-            });
+            Framework.Server.Startup.Configure(app, env);
         }
     }
 }
