fa90c8cad1596f8a2b5c5ede5bdb3c7b8551e0d3
WorkplaceX <info@workplacex.com>
2018-08-06 08:51:13 +0200

    UtilServer.Env

--- Application.Server/Startup.cs
+++ Application.Server/Startup.cs
@@ -20,7 +20,7 @@ namespace Application.Server
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            Framework.Server.Startup.Configure(app, env);
+            Framework.Server.Startup.Configure(app);
         }
     }
 }
