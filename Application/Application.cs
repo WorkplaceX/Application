d4bb1c188371844064a01b5e86e48154fe079e27
WorkplaceX <info@workplacex.com>
2018-08-07 12:10:44 +0200

    Grid

--- Application/Application.cs
+++ Application/Application.cs
@@ -3,16 +3,55 @@
     using Framework.Application;
     using Framework.ComponentJson;
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public class AppHelloWorld : App
     {
+        protected override void Init()
+        {
+            new Button(AppJson) { Text = "Click" };
+            new Button(AppJson) { Text = "Click2" };
+            //
+            Grid grid = new Grid(AppJson);
+            grid.Header = new GridHeader();
+            grid.Header.ColumnList = new List<GridColumn>();
+            grid.Header.ColumnList.Add(new GridColumn() { Text = "Name" });
+            grid.Header.ColumnList.Add(new GridColumn() { Text = "Number", SearchText = "33" });
+            grid.RowList = new List<GridRow>();
+
+            GridRow gridRow0 = new GridRow();
+            gridRow0.IsSelect = true;
+            grid.RowList.Add(gridRow0);
+            gridRow0.CellList = new List<GridCell>();
+            gridRow0.CellList.Add(new GridCell() { Text = "My Text" });
+            gridRow0.CellList.Add(new GridCell() { Text = "22" });
+
+            GridRow gridRow1 = new GridRow();
+            grid.RowList.Add(gridRow1);
+            gridRow1.CellList = new List<GridCell>();
+            gridRow1.CellList.Add(new GridCell() { Text = "Blue green" });
+            gridRow1.CellList.Add(new GridCell() { Text = "2212" });
+        }
+
         protected override void Process()
         {
             base.Process();
             AppJson.Name = "HelloWorld " + DateTime.Now.ToUniversalTime().ToString();
             //
-            new Button(AppJson) { Text = "Click" };
-            new Button(AppJson) { Text = "Click2" };
+            foreach (var grid in AppJson.ListAll().OfType<Grid>())
+            {
+                foreach (var row in grid.RowList)
+                {
+                    row.CellList.ForEach(cell => cell.IsModify = false);
+                    if (row.IsClick)
+                    {
+                        grid.RowList.ForEach(item => item.IsSelect = false);
+                        row.IsClick = false;
+                        row.IsSelect = true;
+                    }
+                }
+            }
         }
     }
 
