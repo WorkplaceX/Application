6e0ed4428e5782fde22ae527a1538d82938fae0c
WorkplaceX <info@workplacex.com>
2018-08-09 23:25:26 +0200

    Dal query

--- Application/App/AppMain.cs
+++ Application/App/AppMain.cs
@@ -19,29 +19,7 @@
             new Button(AppJson) { Text = "Click2" };
             //
             Grid grid = new Grid(AppJson);
-            grid.Header = new GridHeader();
-            grid.Header.ColumnList = new List<GridColumn>();
-            grid.Header.ColumnList.Add(new GridColumn() { Text = "Name" });
-            grid.Header.ColumnList.Add(new GridColumn() { Text = "Number", SearchText = "33" });
-            grid.RowList = new List<GridRow>();
-
-            GridRow gridRow0 = new GridRow();
-            gridRow0.IsSelect = true;
-            grid.RowList.Add(gridRow0);
-            gridRow0.CellList = new List<GridCell>();
-            gridRow0.CellList.Add(new GridCell() { Text = "My Text" });
-            gridRow0.CellList.Add(new GridCell() { Text = "22" });
-
-            GridRow gridRow1 = new GridRow();
-            grid.RowList.Add(gridRow1);
-            gridRow1.CellList = new List<GridCell>();
-            gridRow1.CellList.Add(new GridCell() { Text = "Blue green" });
-            gridRow1.CellList.Add(new GridCell() { Text = "2212" });
-
-            List<Row> list = new List<Row>();
-
-            list.Add(new vAdditionalContactInfo() { FirstName = "Max", LastName = "Meyer" });
-            list.Add(new vAdditionalContactInfo() { FirstName = "John", LastName = "Ford" });
+            var list = UtilDal.Query<vAdditionalContactInfo>().ToList();
 
             grid.Load(this, list);
         }
