64312b5a545b83cacb31bfec79652f1ae6fa9f02
WorkplaceX <info@workplacex.com>
2018-08-10 19:04:48 +0200

    Update for Cli Build and AppSession

--- Application/App/AppMain.cs
+++ Application/App/AppMain.cs
@@ -18,17 +18,39 @@
         {
             new Button(AppJson) { Text = "Click" };
             new Button(AppJson) { Text = "Click2" };
-            //
+
             Grid grid = new Grid(AppJson);
             var list = UtilDal.Query<vAdditionalContactInfo>().ToList();
             grid.Load(this, list);
+
+            var grid2 = new Grid(AppJson);
+            var list2 = UtilDal.Query<Person>().Where(item => item.FirstName == "Kim").ToList();
+            grid2.Load(this, list2);
+        }
+
+        private void ProcessSave()
+        {
+            foreach (var grid in AppJson.ListAll().OfType<Grid>())
+            {
+                int gridIndex = grid.Id;
+                for (int rowIndex = 0; rowIndex < grid.RowList.Count; rowIndex++)
+                {
+                    for (int cellIndex = 0; cellIndex < grid.RowList[rowIndex].CellList.Count; cellIndex++)
+                    {
+                        if (grid.RowList[rowIndex].CellList[cellIndex].IsModify)
+                        { 
+}
+                    }
+                }
+            }
         }
 
         protected override void Process()
         {
-            base.Process();
             AppJson.Name = "HelloWorld " + DateTime.Now.ToUniversalTime().ToString("yyyy-MM-dd HH:mm:ss.fff");
-            //
+
+            ProcessSave();
+
             foreach (var grid in AppJson.ListAll().OfType<Grid>())
             {
                 foreach (var row in grid.RowList)
