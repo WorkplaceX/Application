7c70ad13073beb46b9cd047f619a336a2f86c2b0
WorkplaceX <info@workplacex.com>
2018-12-11 13:37:16 +0100

    Error message if ConfigFramework.json is missing

--- Application.Server/Program.cs
+++ Application.Server/Program.cs
@@ -19,6 +19,8 @@ namespace Application.Server
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .CaptureStartupErrors(true)
+                .UseSetting("detailedErrors", "true")
                 .UseStartup<Startup>()
                 .Build();
     }
